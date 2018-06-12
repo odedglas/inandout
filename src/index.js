205402deefe8c521accd5eb787f5ac619ac7035a
odedglas <odedglas@gmail.com>
2018-06-12 16:33:24 +0300

    Ejecting webpack + config / Adding sass support

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'onsenui/css/onsenui.css';
+import 'onsenui/css/onsen-css-components.css';
 import './assets/style/imports.scss';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
