607458d7be09b044e090c968e6efff6e60f81692
odedglas <odedglas@gmail.com>
2018-06-15 00:43:00 +0300

    Forgot unversioned.

---
+++ src/component/common/Spinner.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Spinner = () =>
+  <svg className={'spinner'} width={'40'} height={'40'} version={'1.1'} xmlns={'http://www.w3.org/2000/svg'}>
+    <circle cx="20" cy="20" r="15" />
+  </svg>;
+
+export default Spinner;
